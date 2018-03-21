57f4d2073b5a93708cd53d48617917572ddec19d
Mosharrof Rubel <rubel007cse@gmail.com>
2018-03-21 09:25:12 +0600

    Modification in 2 files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CSEBOT Project
+
+## This is a test project at seu lab!
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var botBuilder = require(‘claudia-bot-builder’), excuse = require(‘huh’); 
 module.exports = botBuilder(function (request) { 
 
-return ‘Thanks for sending ‘ + request.text + ‘. Your message is very important to us, but ‘ + excuse.get(); 
+return ‘Thanks for sending ‘ + request.text; 
 
 });
